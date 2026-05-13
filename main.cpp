ea66e57ecbfc5e087cb6630a6dab47d4c4c542fc
Muhammad Nauman <maliknomigep@gmail.com>
2026-05-13 09:24:54 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[]={2,3,34,54,654,56};
+    int n=sizeof(arr)/sizeof(int);
+    cout<<n; // to check k arr k ander kitne elements hain r foat k liye / by foat or bakiyon k liye b esse he
+    return 0;
+}
